c294337ae22038d01006d21404572e075060d507
Axel <30324747+AntiDesert5@users.noreply.github.com>
2020-10-23 16:05:44 -0500

    cambios en la estructura

---
+++ keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'seguridadredes.mysql.database.azure.com',
+        user: 'icoico@seguridadredes',
+        password: 'Martin123',
+        database: 'database_rosales',
+    }
+};
--- src/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-        host: 'seguridadredes.mysql.database.azure.com',
-        user: 'icoico@seguridadredes',
-        password: 'Martin123',
-        database: 'database_rosales',
-    }
-};
