ae945eddb82cd6be873b0fb2941b5e9755562d22
Axel V <axelvalenzuela7@gmail.com>
2021-01-28 11:52:42 -0600

    creacion de rama

--- database/db.sql
+++ database/db.sql
@@ -1,18 +1,17 @@
 CREATE DATABASE database_rosales;
 USE database_rosales;
---Users Table
+
 CREATE TABLE users(
     id INT(11) NOT NULL,
     username varchar(16) NOT NULL,
     password varchar(60) NOT NULL,
-    fullname varchar(100) NOT NULL,
-    domicilio varchar(150) NOT NULL
+    fullname varchar(100),
+    domicilio varchar(150) 
 );
 
 ALTER TABLE users ADD PRIMARY KEY(id);
 ALTER TABLE users MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
---DESCRIBE users;
--- Links tables
+
 CREATE TABLE pedidos(
     id INT(11) NOT NULL,
     nombre varchar(150) NOT NULL,
@@ -24,4 +23,3 @@ CREATE TABLE pedidos(
 );
 ALTER TABLE pedidos ADD PRIMARY KEY (id);
 ALTER TABLE pedidos MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
---DESCRIBE pedidos;
